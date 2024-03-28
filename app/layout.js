ebceb8f0f33916d3aa0979ad3a8447cc98643edb
Arian <Arianbod@outlook.com>
2024-03-28 09:35:23 +0300

    apply page styles to layout

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="h-full bg-pLightGrey text-primary font-poppins grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 p-4">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Withdrawn } from '@/components'
 export default function Home() {
   return (
     <main>
-      <div className="h-screen bg-pLightGrey text-primary font-poppins grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 p-4">
+      <div className="">
         <div>
           <Withdrawn />
         </div>
